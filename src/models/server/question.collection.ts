4e3e9a2b0e7ebd60b6b6567f2d44193557675b2c
Polas Chandra Barmon <195308698+devpolas@users.noreply.github.com>
2025-10-28 23:34:00 +0600

    create a question using appwrite node sdk

---
+++ src/models/server/question.collection.ts
@@ -0,0 +1,76 @@
+import { IndexType, Permission } from "node-appwrite";
+import { db, questionCollection } from "../name";
+import { database } from "./config";
+
+export default async function createQuestionCollection() {
+  // create collection
+  await database.createCollection(db, questionCollection, questionCollection, [
+    Permission.read("any"),
+    Permission.read("users"),
+    Permission.write("users"),
+    Permission.update("users"),
+    Permission.delete("users"),
+  ]);
+
+  console.log("Question Collection Created!");
+
+  // create attributes
+
+  await Promise.all([
+    database.createStringAttribute(db, questionCollection, "title", 100, true),
+    database.createStringAttribute(
+      db,
+      questionCollection,
+      "content",
+      10000,
+      true
+    ),
+    database.createStringAttribute(
+      db,
+      questionCollection,
+      "authorId",
+      50,
+      true
+    ),
+    database.createStringAttribute(
+      db,
+      questionCollection,
+      "tags",
+      100,
+      true,
+      undefined,
+      true
+    ),
+    database.createStringAttribute(
+      db,
+      questionCollection,
+      "attachmentId",
+      100,
+      true
+    ),
+    database.createStringAttribute(db, questionCollection, "title", 50, false),
+  ]);
+
+  console.log("Question Attributes Created!");
+
+  // create index
+
+  await Promise.all([
+    database.createIndex(
+      db,
+      questionCollection,
+      "title",
+      IndexType.Fulltext,
+      ["title"],
+      ["asc"]
+    ),
+    database.createIndex(
+      db,
+      questionCollection,
+      "content",
+      IndexType.Fulltext,
+      ["content"],
+      ["asc"]
+    ),
+  ]);
+}
